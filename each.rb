cc1e0df0dbf4d1e4870224c7032e444911b2c85e
Caleb Thomas <cjthomas00@comcast.net>
2022-10-19 15:10:29 -0600

    complete challenge 2

--- each.rb
+++ each.rb
@@ -15,7 +15,9 @@ weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
 words.each do |word|
-  puts words.max {|word| word.length = 3 }
+  if word.length == 3
+    puts word
+  end
 end
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
